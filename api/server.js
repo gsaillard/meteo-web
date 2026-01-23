dee562151c12630978f131505b62d52e56246f52
LeLeIl <leilachakib@hotmail.com>
2026-01-23 10:19:00 +0100

    j

---
+++ api/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+const app = express();
+app.use(express.json());
+
+// Base des donnes SQLite
+const db = new sqlite3.Database("data.db");
+
+// Create a table
+db.run(`
+  CREATE TABLE IF NOT EXISTS mesures (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    valeur REAL
+  )
+`);
+
+// Endpoint de test
+app.get("/ping", (req, res) => {
+  res.json({ message: "API OK" });
+});
+
+// enregistrer la valeur
+app.post("/save", (req, res) => {
+  const valeur = req.body.valeur;
+
+  db.run(
+    "INSERT INTO mesures(valeur) VALUES (?)",
+    [valeur],
+    () => res.json({ status: "saved", valeur })
+  );
+});
+
+app.listen(3000, () => {
+  console.log("API Node.js escuchando en puerto 3000");
+});
